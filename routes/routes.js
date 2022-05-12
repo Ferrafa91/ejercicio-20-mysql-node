e2a9de3f8dff112aa0fe725b9cda32ce113b2c49
Fernando Bogorja <fbogorja@gmail.com>
2022-05-12 16:46:03 -0300

    Ruta editar agregada

--- routes/routes.js
+++ routes/routes.js
@@ -41,4 +41,33 @@ router.post("/crear", (req, res) => {
   );
 });
 
+router.get("/editar/:id", (req, res) => {
+  res.render("editar");
+});
+
+router.post("/editar/:id", (req, res) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root1234",
+    database: "ha_ejercicio_20",
+  });
+  connection.query(
+    "UPDATE users SET firstname=?,lastname=?,age=? WHERE ID=?",
+    [
+      req.body.createName,
+      req.body.createLastName,
+      req.body.createAge,
+      req.params.id,
+    ],
+    function (error, response) {
+      if (error) {
+        throw error;
+      } else {
+        res.redirect("/usuarios");
+      }
+    }
+  );
+});
+
 module.exports = router;
