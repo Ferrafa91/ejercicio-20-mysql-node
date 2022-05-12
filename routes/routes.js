0f2d9f450954187f4a2be880479c4621b250f056
Fernando Bogorja <fbogorja@gmail.com>
2022-05-12 17:31:50 -0300

    ruta eliminar realizada

--- routes/routes.js
+++ routes/routes.js
@@ -70,4 +70,24 @@ router.post("/editar/:id", (req, res) => {
   );
 });
 
+router.get("/eliminar/:id", (req, res) => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root1234",
+    database: "ha_ejercicio_20",
+  });
+  connection.query(
+    "DELETE FROM users WHERE id=?",
+    [req.params.id],
+    function (error, response) {
+      if (error) {
+        throw error;
+      } else {
+        res.redirect("/usuarios");
+      }
+    }
+  );
+});
+
 module.exports = router;
